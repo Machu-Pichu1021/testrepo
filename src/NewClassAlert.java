e93f5283f8b775d7ec9934c87b0d88ba857836f7
BT_1N3_12 <sample@bths.edu>
2025-01-10 13:53:50 -0500

    yes indeed

--- src/NewClassAlert.java
+++ src/NewClassAlert.java
@@ -5,4 +5,7 @@ public class NewClassAlert {
     public void newSigmaMode(){
         System.out.println("the ohians don't know about this");
     }
+    public int numberOfOhians(int reeeee){
+        return reeeee+400;
+    }
 }
