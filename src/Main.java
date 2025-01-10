40392d7db50ab5b5ff76df431657499b5f7aa6dd
Machu-Pichu1021 <matthewr570@nycstudents.net>
2025-01-10 13:47:59 -0500

    Major change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Ultra mega change incoming");
         System.out.println("omega monkey");
 
         System.out.println("MOAR PRINTSSSSSSS");
